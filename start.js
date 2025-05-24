142d9ed96cab0faafa91ac2704d724a016b89ecf
Pritam Kumar Sahu <pritamkumarsahu999@gamil.com>
2025-05-24 09:49:33 +0530

    complete the guessing game

--- start.js
+++ start.js
@@ -1,1 +1,13 @@
-//feature
+const favMovie = "avatar";                           //setting up a variable
+
+let guess = prompt("Guess my favorite movie");       //giving pronpt to take the requests
+
+while((guess  = favMovie) && (guess  = "quit")){     //conditions for guess
+    guess = prompt("wrong guess please try again");  //if guess is not equal to favMovie then return the prompt 
+} 
+if(guess == favMovie){                               //condition if guess is true
+    console.log("congrats !!");                      //winner!!!
+}else{
+    console.log("You quit");                         //you typed guess = quit
+}
+ 
